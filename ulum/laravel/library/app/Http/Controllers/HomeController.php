9fbbc3a924d1f9cc6ec3c4d5fb8cdb7d80e9d42c
Syahrul Ulum <sahrululum73@gmail.com>
2021-12-14 12:06:09 +0700

    add query builder

--- ulum/laravel/library/app/Http/Controllers/HomeController.php
+++ ulum/laravel/library/app/Http/Controllers/HomeController.php
@@ -30,6 +30,132 @@ class HomeController extends Controller
      */
     public function index()
     {
+
+        // data1
+        $data1 = Member::select('*')
+            ->rightJoin('users', 'users.member_id', 'members.id')
+            ->get();
+
+        // data2
+        $data2 = Member::select('*')
+            ->leftJoin('users', 'users.member_id', 'members.id')
+            ->where('users.id', NULL)
+            ->get();
+
+        //data 3
+        $data3 = Member::select('members.id', 'members.name')
+            ->leftJoin('transactions', 'transactions.member_id', 'members.id')
+            ->where('transactions.id', NULL)
+            ->get();
+
+        //data 4
+        $data4 = Member::select('members.id', 'members.name', 'members.phone_number')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->get();
+
+        // data5
+        $data5 = Member::select('members.id', 'members.name', 'members.phone_number')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->groupBy('members.id', 'members.name', 'members.phone_number')
+            ->havingRaw('COUNT(*) > ?', [1])
+            ->get();
+
+        //data6
+        $data6 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->get();
+
+        //data7
+        $data7 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->whereMonth('date_end', 6)
+            ->get();
+
+        //data8
+        $data8 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->whereMonth('date_end', 5)
+            ->get();
+
+        //data9
+        $data9 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->whereMonth('date_end', 6)
+            ->get();
+
+        //data10
+        $data10 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->where('members.address', 'LIKE', '%bandung%')
+            ->get();
+
+        //data11
+        $data11 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->where('members.address', 'LIKE', '%bandung%')
+            ->where('members.gender', '=', 'F')
+            ->get();
+
+        //data12
+        $data12 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'books.isbn', 'transaction_details.qty')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->rightJoin('transaction_details', 'transaction_details.transaction_id', 'transactions.id')
+            ->rightJoin('books', 'books.id', 'transaction_details.book_id')
+            ->groupBy('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'books.isbn', 'transaction_details.qty')
+            ->havingRaw('SUM(transaction_details.qty) > ?', [1])
+            ->get();
+
+        //data13
+        $data13 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'books.isbn', Member::raw(('transaction_details.qty * books.price as total_price')))
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->rightJoin('transaction_details', 'transaction_details.transaction_id', 'transactions.id')
+            ->leftJoin('books', 'books.id', 'transaction_details.book_id')
+            ->get();
+
+        //data14
+        $data14 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'books.isbn', 'transaction_details.qty', 'books.title as book_title', 'authors.name as author_name', 'publishers.name as publisher_name', 'catalogs.name as catalog_name')
+            ->rightJoin('transactions', 'transactions.member_id', 'members.id')
+            ->rightJoin('transaction_details', 'transaction_details.transaction_id', 'transactions.id')
+            ->leftJoin('books', 'books.id', 'transaction_details.book_id')
+            ->leftJoin('publishers', 'publishers.id', 'books.publisher_id')
+            ->leftJoin('authors', 'authors.id', 'books.author_id')
+            ->leftJoin('catalogs', 'catalogs.id', 'books.catalog_id')
+            ->get();
+
+        //data15
+        $data15 = Catalog::select('catalogs.*', 'books.title as book_title')
+            ->rightJoin('books', 'books.catalog_id', 'catalogs.id')
+            ->get();
+
+        //data16
+        $data16 = Book::select('books.*', 'publishers.name as publisher_name')
+            ->leftJoin('publishers', 'publishers.id', 'books.publisher_id')
+            ->get();
+
+        //data17
+        $data17 = Author::select('*')
+            ->where('authors.id', '=', 5)
+            ->get();
+
+        //data18
+        $data18 = Book::select('*')
+            ->where('price', '>', 10000)
+            ->get();
+
+        //data19
+        $data19 = Book::select('*')
+            ->where('publisher_id', '=', 1)
+            ->where('qty', '>', 10)
+            ->get();
+
+        //data20
+        $data20 = Member::select('*')
+            ->whereMonth('created_at', 6)
+            ->get();
+
+        return $data20;
+
+
         return view('home');
     }
 }
