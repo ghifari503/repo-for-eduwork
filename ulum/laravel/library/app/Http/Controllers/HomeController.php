376bf5f36134942f20530efda0e9fa6494cb0824
Syahrul Ulum <sahrululum73@gmail.com>
2021-12-17 18:31:38 +0700

    Add Grafik

--- ulum/laravel/library/app/Http/Controllers/HomeController.php
+++ ulum/laravel/library/app/Http/Controllers/HomeController.php
@@ -156,6 +156,42 @@ class HomeController extends Controller
         // return $data20;
 
 
-        return view('home');
+        $total_books = Book::count();
+        $total_members = Member::count();
+        $total_publishers = Publisher::count();
+        $total_authors = Author::count();
+
+        $data_donut = Book::select(Book::raw("COUNT(publisher_id) as total"))
+            ->groupBy('publisher_id')
+            ->orderBy('publisher_id', 'ASC')
+            ->pluck('total');
+
+        $label_donut = Publisher::orderBy('publishers.id', 'asc')
+            ->join('books', 'books.publisher_id', 'publishers.id')
+            ->groupBy('publishers.name')
+            ->pluck('publishers.name');
+
+        $label_bar = ['Borrowing', 'Return'];
+        $data_bar = [];
+
+        foreach ($label_bar as $key => $value) {
+            $data_bar[$key]['label'] = $label_bar[$key];
+            $data_bar[$key]['backgroundColor'] = $key == 1 ? 'rgba(60,141,188,0.9)' : 'rgba(210,214,222,1)';
+            $data_month = [];
+
+            foreach (range(1, 12) as $month) {
+                if ($key == 0) {
+                    $data_month[] = Transaction::select(Transaction::raw("COUNT(*) as total"))
+                        ->whereMonth('date_start', $month)->first()->total;
+                } else {
+                    $data_month[] = Transaction::select(Transaction::raw("COUNT(*) as total"))
+                        ->whereMonth('date_end', $month)->first()->total;
+                }
+            }
+
+            $data_bar[$key]['data'] = $data_month;
+        }
+
+        return view('home', compact('total_books', 'total_members', 'total_publishers', 'total_authors', 'data_donut', 'label_donut', 'data_bar'));
     }
 }
