8d0120bad213369b548bf263e4cc5f7cd66f40d9
awalrizqifr <awalrizqifr@gmail.com>
2021-12-22 12:53:44 +0700

    Create helper convert to rupiah

--- awal/laravel/library/app/Helpers/helpers.php
+++ awal/laravel/library/app/Helpers/helpers.php
@@ -4,3 +4,8 @@ function convert_date($value)
 {
     return date('d M Y - H:i:s', strtotime($value));
 }
+
+function convert_to_rupiah($number)
+{
+    return 'Rp. ' . strrev(implode('.', str_split(strrev(strval($number)), 3)));
+}
