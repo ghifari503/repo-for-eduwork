467e5aa4b2bbaeb06fbdf066f5d00def1e81d66d
awalrizqifr <awalrizqifr@gmail.com>
2021-12-23 10:48:11 +0700

    Create and use notifications helpers for showing late borrow data

--- awal/laravel/library/app/Helpers/helpers.php
+++ awal/laravel/library/app/Helpers/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Transaction;
+use Illuminate\Support\Facades\DB;
+
 function convert_date($value)
 {
     return date('d M Y - H:i:s', strtotime($value));
@@ -8,4 +11,11 @@ function convert_date($value)
 function convert_to_rupiah($number)
 {
     return 'Rp. ' . strrev(implode('.', str_split(strrev(strval($number)), 3)));
+}
+
+function notifications()
+{
+   $notifications = Transaction::with('member')->select('transactions.*', DB::raw('DATEDIFF(CURRENT_DATE, transactions.date_end) as late_days'))->where('status', 0)->get();
+
+   return $notifications;
 }
--- awal/laravel/library/resources/views/layouts/admin.blade.php
+++ awal/laravel/library/resources/views/layouts/admin.blade.php
@@ -166,6 +166,35 @@
                             </div>
                         </li>
 
+                        <!-- Nav Item - Alerts -->
+                        <li class="nav-item dropdown no-arrow mx-1">
+                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-bell fa-fw"></i>
+                                <!-- Counter - Alerts -->
+                                <span class="badge badge-danger badge-counter">{{ count(notifications()) }}</span>
+                            </a>
+                            <!-- Dropdown - Alerts -->
+                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                                aria-labelledby="alertsDropdown">
+                                <h6 class="dropdown-header">
+                                    Notifications
+                                </h6>
+                                @foreach(notifications() as $notification)
+                                    <a class="dropdown-item d-flex align-items-center" href="#">
+                                        <div class="mr-3">
+                                            <div class="icon-circle bg-primary">
+                                                <i class="fas fa-file-alt text-white"></i>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <span class="font-weight-bold">{{ $notification->member->name }} it's {{ $notification->late_days }} days late</span>
+                                        </div>
+                                    </a>
+                                @endforeach
+                            </div>
+                        </li>
+
                         <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
