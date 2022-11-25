feef96b9aa86d120eba59ca24be02779158ee982
ghifari503 <ghifari.v.putra@gmail.com>
2022-11-25 19:16:36 +0700

    tugas_php

---
+++ ghifari/tugas_php/bangun_ruang.php
@@ -0,0 +1,33 @@
+<?php
+$s = 11;
+$r =3;
+
+echo "1. Diketahui suatu kubus dengan panjang rusuk sepanjang 11 cm. Hitunglah luas permukaan dan volume kubus tersebut.<br/>";
+echo "<br>";
+$L=  6*pow($s,2);
+echo "6 * ($s*$s) = $L";
+echo "<br>";
+echo "luas permukaan= &nbsp;" .$L;
+echo "<br>";
+echo "<br>";
+echo "Menghitung Volume";
+echo "<br>";
+echo "<br>";
+$V= pow($s,3);
+echo "($s*$s*$s) = $V";
+echo "<br>";
+echo "volume= &nbsp;" .$V;
+echo "<hr>";
+
+echo "2. Tentukan volume sebuah kubus yang memiliki rusuknya 6 m!<br/>";
+echo "<br>";
+$v= pow($r,3);
+echo "V&nbsp($r*$r*$r) =$v";
+
+
+
+
+
+
+
+?>
