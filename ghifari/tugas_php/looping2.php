368f56e30fa83a952bf4c40adab30adff3da9fa4
ghifari503 <ghifari.v.putra@gmail.com>
2022-11-29 16:55:47 +0700

    looping.php

---
+++ ghifari/tugas_php/looping2.php
@@ -0,0 +1,22 @@
+<?php
+
+$no = 1;
+$nama = 1;
+
+
+for ($no = 1; $no <= 10; $no++) {
+   echo " $no <br>";
+}
+for ($nama = 1; $nama <= 10; $nama++) {
+   echo "Nama ke: $nama <br>";
+ }  
+ for ($i=10; $i >0 ; $i--) { 
+  echo "kelas $i <br>";
+ }
+ 
+   
+
+
+
+?>
+
