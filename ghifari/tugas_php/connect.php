e13dd9524be8538743a46100458d1237d43d3b00
ghifari503 <ghifari.v.putra@gmail.com>
2022-11-30 22:46:36 +0700

    connect

--- ghifari/tugas_php/connect.php
+++ ghifari/tugas_php/connect.php
@@ -11,18 +11,8 @@ if (!$conn) {
     die("connection failed:" . mysqli_connected_error());
 }
 
-// echo "connected successfully";
-// mysqli_close($conn);
+echo "connected successfully";
+mysqli_close($conn);
 
-$sql = "SELECT * FROM buku";
-$result = $conn->query($sql);
 
-if ($result->num_row >0){
-    while($row = $result->fetch_assoc()){
-        echo "buku : " . $row["isbn"]. " ". $row["judul"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
 ?>
