af5932c53198597b9375a4b3d1fe694a187f9b09
ghifari503 <ghifari.v.putra@gmail.com>
2022-12-12 22:51:20 +0700

    route and view

---
+++ ghifari/library/resources/views/admin/author/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+ini adalah halaman Author
+    
+@endsection
---
+++ ghifari/library/resources/views/admin/publisher/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+ini adalah halaman Catalog
+    
+@endsection
